ce1d5e584cb5b3209d779455b2d64066c2842e40
Menne Bos <mennebos@gmail.com>
2025-07-27 14:04:06 +0200

    update setup according tech docs

--- setup.py
+++ setup.py
@@ -1,36 +1,14 @@
-# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
-# All trademark and other rights reserved by their respective owners
-# Copyright 2008-2025 Neongecko.com Inc.
-# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
-# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
-# BSD-3 License
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 1. Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-# 3. Neither the name of the copyright holder nor the names of its
-#    contributors may be used to endorse or promote products derived from this
-#    software without specific prior written permission.
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 from setuptools import setup, find_packages
-from os import path, getenv
+from os import path
 
-BASE_PATH = path.abspath(path.dirname(__file__))
+PLUGIN_TYPE = "ovos.plugin.phal"  # Adjust based on the plugin type
+PLUGIN_NAME = "ovos-plugin-gpio"
+PLUGIN_PKG = PLUGIN_NAME.replace("-", "_")
+PLUGIN_CLAZZ = "SwitchInputs" # same is class name in __init__.py
+PLUGIN_CONFIGS = "MyPluginConfig"
 
+PLUGIN_ENTRY_POINT = f'{PLUGIN_NAME} = {PLUGIN_PKG}:{PLUGIN_CLAZZ}'
+CONFIG_ENTRY_POINT = f'{PLUGIN_NAME}.config = {PLUGIN_PKG}:{PLUGIN_CONFIGS}'
 
 def get_requirements(requirements_filename: str):
     requirements_file = path.join(BASE_PATH, "requirements",
@@ -41,35 +19,12 @@ def get_requirements(requirements_filename: str):
                     not r.strip().startswith("#")]
     return requirements
 
-
-with open(path.join(BASE_PATH, "README.md"), "r") as f:
-    long_description = f.read()
-
-with open(path.join(BASE_PATH, "version.py"), "r",
-          encoding="utf-8") as v:
-    for line in v.readlines():
-        if line.startswith("__version__"):
-            if '"' in line:
-                version = line.split('"')[1]
-            else:
-                version = line.split("'")[1]
-
 setup(
-    name='ovos-plugin-gpio',
-    version=version,
-    license='BSD-3',
-    author='Menne Bos',
-    author_email='',
-    url='https://github.com/MenneBos/ovos-plugin-gpio',
-    description='GPIO Interface',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    name=PLUGIN_NAME,
+    version='0.1.0',
+    packages=[PLUGIN_PKG],
     install_requires=get_requirements('requirements.txt'),
-    packages=find_packages(),
-    include_package_data=True,
-    entry_points={
-        'ovos.plugin.phal': [
-            "voice_relay-phal-plugin-switches = voice_relay_phal_plugin_switches:SwitchInputs"
-        ]
-    }
+    keywords='ovos plugin phal gpio',
+    entry_points={PLUGIN_TYPE: PLUGIN_ENTRY_POINT, f'{PLUGIN_TYPE}.config': CONFIG_ENTRY_POINT}
 )
+
