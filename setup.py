92d7940c96ff5e68f4adb8933c4eb5e84b9ba5ac
Menne Bos <mennebos@gmail.com>
2025-07-27 20:49:02 +0200

    changed entry point

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os import path
 
 PLUGIN_TYPE = "ovos.plugin.phal"  # Adjust based on the plugin type
-PLUGIN_NAME = "ovos-plugin-gpio"
+PLUGIN_NAME = "voice_relay-phal-plugin-switches"
 PLUGIN_PKG = PLUGIN_NAME.replace("-", "_")
 PLUGIN_CLAZZ = "SwitchInputs" # same is class name in __init__.py
 PLUGIN_CONFIGS = "MyPluginConfig"
